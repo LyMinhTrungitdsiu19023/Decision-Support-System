7781fefa6d4976cfa98adf9fb8ab1cab4877c495
Ly Minh Trung <trung3566@gmail.com>
2022-10-18 12:59:52 +0700

    Update DSS_project.py

--- DSS_project.py
+++ DSS_project.py
@@ -17,7 +17,7 @@ Truong Quoc An
 
 st.sidebar.header('Squad Selection')
 # Sidebar - Position selection
-unique_pos = ['GK','DF','MF','FW']
+unique_pos = ['GK','DF','MF','FW', 'DF,MF', 'MF,FW', 'FW,DF']
 selected_pos = st.sidebar.multiselect('Posision', unique_pos, unique_pos)
 
 

8db31f2e510020720cad9e6476dfec326b28265b
Ly Minh Trung <trung3566@gmail.com>
2022-10-18 13:07:33 +0700

    Update DSS_project.py

--- DSS_project.py
+++ DSS_project.py
@@ -35,16 +35,25 @@ playerstats = load_data()
 unique_nation = playerstats["Nation"].tolist()
 selected_nation = st.sidebar.multiselect('Nation', unique_nation, unique_nation)
 
-df_selected_position= playerstats[(playerstats.Pos.isin(selected_pos)) & (playerstats.Nation.isin(selected_nation))]
+df_selected_position = playerstats[(playerstats.Pos.isin(selected_pos))] 
+
+df_selected_nation = playerstats[(playerstats.Pos.isin(selected_pos)) & (playerstats.Nation.isin(selected_nation))]
+
 
 st.header("Information of Manchester City's Players")
 st.write('Data Dimension: ' + str(playerstats.shape[0]) + ' rows and ' + str(playerstats.shape[1]) + ' columns.')
 st.dataframe(playerstats)
 
 
-# Heatmap
+# button
 if st.button('View Players by Position'):
     st.header('Players by Position')
     st.write('Data Dimension: ' + str(df_selected_position.shape[0]) + ' rows and ' + str(df_selected_position.shape[1]) + ' columns.')
 
-    st.dataframe(df_selected_position)
+    st.dataframe(df_selected_position) 
+    
+if st.button('View Players by Nation'):
+    st.header('Players by Nation')
+    st.write('Data Dimension: ' + str(df_selected_nation.shape[0]) + ' rows and ' + str(df_selected_nation.shape[1]) + ' columns.')
+
+    st.dataframe(df_selected_nation) 

85a47bf21b5e3b6c8bf2222ab69a780b2ad4ec3a
Ly Minh Trung <trung3566@gmail.com>
2022-10-18 18:55:26 +0700

    Update DSS_project.py

--- DSS_project.py
+++ DSS_project.py
@@ -75,16 +75,25 @@ if st.button('Squad Suggestion'):
 #Forward
     st.header('Analysis of Forward')
     fw = pd.read_html(url, header = 1)
+    
+
     fw_table = fw[4]
     fw_table.drop(fw_table.tail(2).index, inplace = True)
     fw_table = fw_table.loc[fw_table["Pos"].str.contains("FW")]
     fw_table["Nation"] = fw_table["Nation"].str.replace('[a-z]', '')
     
-    fw_table = fw_table.drop(['SoT%', 'Sh/90', 'SoT/90', 'G/SoT', 'Dist', 'xG', 'npxG', 'npxG/Sh', 'G-xG', 'np:G-xG', 'Matches'], axis=1)
-    fw_table.rename(columns = {'Gls':'Goals', 'Sh':'Shots total', 'SoT':'Shots on Target', 'G/Sh':'Goal per Shot', 'FK':'Freekick','PK':'Penalty Kick','PKatt':'Pentallty Attemp'}, inplace = True)
-    fw_table = fw_table.reset_index(drop = True)
+    #Shoot
+    fw_shoot = fw_table.drop(['SoT%', 'Sh/90', 'SoT/90', 'G/SoT', 'Dist', 'xG', 'npxG', 'npxG/Sh', 'G-xG', 'np:G-xG', 'Matches'], axis=1)
+    fw_shoot.rename(columns = {'Gls':'Goals', 'Sh':'Shots total', 'SoT':'Shots on Target', 'G/Sh':'Goal per Shot', 'FK':'Freekick','PK':'Penalty Kick','PKatt':'Pentallty Attemp'}, inplace = True)
+    fw_shoot = fw_shoot.reset_index(drop = True)
     st.write('Stats of Shooting')
-    st.dataframe(fw_table) 
+    st.dataframe(fw_shoot) 
     
+    #Pass
+    fw_pass = fw_table.drop(['xA', 'A-xA', 'KP', '1/3', 'PPA', 'CrsPA', 'Prog','Matches'], axis=1)
+#     fw_pass.rename(columns = {'Gls':'Goals', 'Sh':'Shots total', 'SoT':'Shots on Target', 'G/Sh':'Goal per Shot', 'FK':'Freekick','PK':'Penalty Kick','PKatt':'Pentallty Attemp'}, inplace = True)
+    fw_pass = fw_pass.reset_index(drop = True)
+    st.write('Stats of Passing')
+    st.dataframe(fw_pass) 
     
  

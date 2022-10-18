cf7a3d2f7b2f7b14a7a8f53086fe435f1b3bbb35
Ly Minh Trung <trung3566@gmail.com>
2022-10-18 15:45:05 +0700

    Update DSS_project.py

--- DSS_project.py
+++ DSS_project.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 image = Image.open('logo.jpg')
 
-st.image(image, caption='The Citizen', width=400)
+st.image(image, caption='The Citizen', width=100)
 
 st.title('Manchester City Football Club Management Decision Support System')
 

ece1041f4f13bffabecfc7d59f988a05616db796
Ly Minh Trung <trung3566@gmail.com>
2022-11-15 23:23:35 +0700

    Create Mancity_Transfer.py

---
+++ Mancity_Transfer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+from PIL import Image
+
+st.set_page_config(page_title="Transfer", layout = 'wide')

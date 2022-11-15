01e62858943f179de034bcbdb90f47bf379a0976
Ly Minh Trung <trung3566@gmail.com>
2022-11-15 20:29:32 +0700

    Update DSS_project.py

--- DSS_project.py
+++ DSS_project.py
@@ -394,7 +394,7 @@ def prediction(url):
     
     predic_df = pd.merge(exshoot, expassing, on='Player', how='inner')
 
-    st.dataframe(predic_df, width=2500, height=768)
+    st.dataframe(predic_df, width=2500, height=400)
     
 #button 
 # if st.button('Squad Analysis'):

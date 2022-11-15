a643a0803171c6471348a1520edb7189e52db42c
Ly Minh Trung <trung3566@gmail.com>
2022-11-15 20:34:47 +0700

    Update DSS_project.py

--- DSS_project.py
+++ DSS_project.py
@@ -394,7 +394,7 @@ def prediction(url):
     predic_df = pd.merge(exshoot, expassing, on='Player', how='inner')
     predic_df.rename(columns = {'xG':'Expected Goals', 'npxG':'NonPenalty Expected Goals', 'npxG/Sh':'NonPenalty Expected Goals/shots', 'G-xG':'Goals compare ExGoals', 'np:G-xG':'NonPen Goal compare with expected', 'xAG':'Expected Assist Goals', 'xA':'Expected Assist'}, inplace = True)
 
-    st.dataframe(predic_df, width=2500, height=400)
+    st.dataframe(predic_df, width=2500, height=400, use_container_width=True)
     
 #button 
 # if st.button('Squad Analysis'):

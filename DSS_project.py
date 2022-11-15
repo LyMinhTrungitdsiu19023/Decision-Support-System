da993013d19351cd698c5c6f910b61aa4b088033
Ly Minh Trung <trung3566@gmail.com>
2022-11-15 22:26:04 +0700

    Update DSS_project.py

--- DSS_project.py
+++ DSS_project.py
@@ -24,7 +24,8 @@ st.sidebar.header('Features')
 
 
 url = "https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats"
-@st.cache
+# @st.cache
+@st.cache(cache_by_id=[spacy.lang.en.English])
 def load_data(url):
 
     html = pd.read_html(url, header = 1)

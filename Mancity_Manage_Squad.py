d4ef728134704f7c68457703c65d75f5bb43e433
Ly Minh Trung <trung3566@gmail.com>
2022-11-19 13:02:37 +0700

    Update Mancity_Manage_Squad.py

--- Mancity_Manage_Squad.py
+++ Mancity_Manage_Squad.py
@@ -44,8 +44,8 @@ def load_data(url):
 #     possesion = html[9]
     playerstats.drop(playerstats.tail(2).index, inplace = True)
     playerstats["Nation"] = playerstats["Nation"].str.replace('[a-z]', '')
-#     playerstats["Pos"] = playerstats["Pos"].str.replace('FW,DF', 'FW')
-
+    
+    playerstats = playerstats.str.replace('FW,DF', 'FW')
 
     return playerstats, shoot, passing, df
 # playerstats = load_data(url)[0]

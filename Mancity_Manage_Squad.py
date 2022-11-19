2e3a8234c43521186655ff7bf2310e7072f4fdc0
Ly Minh Trung <trung3566@gmail.com>
2022-11-19 10:21:12 +0700

    Update Mancity_Manage_Squad.py

--- Mancity_Manage_Squad.py
+++ Mancity_Manage_Squad.py
@@ -34,13 +34,9 @@ url = "https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats"
 @st.cache(allow_output_mutation=True)
 
 
-def clean_html(raw_html):
-    cleaner = Cleaner(remove_tags=["sup"])
-    return cleaner.clean_html(raw_html).decode("utf-8")
-
 def load_data(url):
 
-    html = clean_html(pd.read_html(url, header = 1))
+    html = pd.read_html(url, header = 1)
     playerstats = html[0]
     shoot = html[4]
     passing = html[5]

49c13fb9eb311cd835c06bab5cb49ba40d9f4c20
Ly Minh Trung <trung3566@gmail.com>
2022-11-22 21:19:10 +0700

    Update Mancity_Manage_Squad.py

--- Mancity_Manage_Squad.py
+++ Mancity_Manage_Squad.py
@@ -856,8 +856,9 @@ def filter_player_by_sidebar(url, url_transfer, url_defend, url_gk,player_name, 
             pass
         else:
             playerlist = playerlist.loc[playerlist["Comp"].str.contains(str(league))] 
+        layerlist = playerlist[playerlist["Player"].str.contains("Player")==False]
+
         playerlist = playerlist.sort_values(by='GA', ascending=False)
-        playerlist = playerlist[playerlist["Player"].str.contains("Player")==False]
         playerlist = playerlist.head(10)
         playerlist = playerlist.reset_index(drop = True)
 

4fbc9e007538414984f07e4788ead15bd40fe0ec
Ly Minh Trung <trung3566@gmail.com>
2022-11-22 20:01:55 +0700

    Update Mancity_Manage_Squad.py

--- Mancity_Manage_Squad.py
+++ Mancity_Manage_Squad.py
@@ -814,7 +814,7 @@ def filter_player_by_sidebar(url, url_transfer, url_defend, player_name, league)
     cosine_lst = []
     my_player = load_data(url)[0].loc[load_data(url)[0]["Player"] == player_name]
     my_player = my_player[['Player','Nation','Pos','Age','Gls','Ast','xG','xAG']]
-    my_player = pd.concat([my_player, Analysis(url)[2][['TklW','Int']]], axis = 1)
+    my_player = pd.concat([my_player, Analysis(url)[2][['TklW','Intercept']]], axis = 1)
     
     playerlist = get_data(url_transfer)[['Player','Nation','Pos','Age','Squad','Comp','Gls','Ast','xG','xAG']] #All players
     playerlist = pd.concat([playerlist, get_player_defend_table(url_defend)], axis=1)

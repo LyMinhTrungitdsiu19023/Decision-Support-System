6cb2e161e8873e788074f4810a16c4d405d9c7c0
kieuhuy <83636991+kieuhuy@users.noreply.github.com>
2023-05-03 16:29:16 +0700

    Update Mancity_Manage_Squad.py

--- Mancity_Manage_Squad.py
+++ Mancity_Manage_Squad.py
@@ -25,9 +25,10 @@ st.set_page_config(page_title="Manchester City Decision Support System", layout 
 # url = call_api("https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats")
 # url = "https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats"
 # url_transfer = "https://fbref.com/en/comps/Big5/stats/players/Big-5-European-Leagues-Stats"
-@st.cache_data(experimental_allow_widgets=True)
+
 url = "https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats"
 url_transfer = "https://fbref.com/en/comps/Big5/stats/players/Big-5-European-Leagues-Stats"
+@st.cache_data(experimental_allow_widgets=True)
 def load_data(url):
     html = pd.read_html(url, header = 1)
     playerstats = html[0]
